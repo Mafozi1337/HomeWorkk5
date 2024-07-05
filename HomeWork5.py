7c675aaba9303610a19f56f2171f7e9e18011d54
Mafozi1337 <93876351+Mafozi1337@users.noreply.github.com>
2024-07-06 00:53:52 +0700

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,13 @@
+my_list = ['Apple', 'Melon', 'Banana', 'Orange', 'Pineapple']
+print(my_list)
+print(my_list[0::4])
+print(my_list[3:5])
+my_list[3] = 'FakeOrange'
+print(my_list)
+my_dict = {'Apple': 'Яблоко', 'Banana': 'Банан', 'Orange': 'Апельсин'}
+print(my_dict)
+print(my_dict['Apple'])
+my_dict['Apple'] = 'Мандаринка'
+print(my_dict)
+my_dict.update({'Melon': 'Дыня'})
+print(my_dict)
